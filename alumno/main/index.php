64060d05d03fa2c82b975c8704fcbc2e08689042
Jorge <jorgehn1995@gmail.com>
2018-05-03 11:22:13 -0600

    Iniciando con vs code

--- alumno/main/index.php
+++ alumno/main/index.php
@@ -1,10 +1,11 @@
-<?php include '../lib/sesion.php'; ?>
+<?php include '../lib/sesion.php';?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8" />
-  <?php $titulo="Inicio Alumnos";
-  include '../lib/header.php'; ?>
+  <?php $titulo = "Inicio Alumnos";
+include '../lib/header.php';?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta content="Sistema para el control de notas escolares" name="description" />
   <meta content="Jorge Hernandez" name="author" />
@@ -19,154 +20,172 @@
 
   <script src="../../assets/js/modernizr.min.js"></script>
   <style media="screen">
-  .rounded-circle, .head{
+    .rounded-circle,
+    .head {
 
-    background: rgba(59,153,156,1);
-    background: -moz-linear-gradient(left, rgba(59,153,156,1) 0%, rgba(0,177,156,1) 100%);
-    background: -webkit-gradient(left top, right top, color-stop(0%, rgba(59,153,156,1)), color-stop(100%, rgba(0,177,156,1)));
-    background: -webkit-linear-gradient(left, rgba(59,153,156,1) 0%, rgba(0,177,156,1) 100%);
-    background: -o-linear-gradient(left, rgba(59,153,156,1) 0%, rgba(0,177,156,1) 100%);
-    background: -ms-linear-gradient(left, rgba(59,153,156,1) 0%, rgba(0,177,156,1) 100%);
-    background: linear-gradient(to right, rgba(59,153,156,1) 0%, rgba(0,177,156,1) 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3b999c', endColorstr='#00b19c', GradientType=1 );
+      background: rgba(59, 153, 156, 1);
+      background: -moz-linear-gradient(left, rgba(59, 153, 156, 1) 0%, rgba(0, 177, 156, 1) 100%);
+      background: -webkit-gradient(left top, right top, color-stop(0%, rgba(59, 153, 156, 1)), color-stop(100%, rgba(0, 177, 156, 1)));
+      background: -webkit-linear-gradient(left, rgba(59, 153, 156, 1) 0%, rgba(0, 177, 156, 1) 100%);
+      background: -o-linear-gradient(left, rgba(59, 153, 156, 1) 0%, rgba(0, 177, 156, 1) 100%);
+      background: -ms-linear-gradient(left, rgba(59, 153, 156, 1) 0%, rgba(0, 177, 156, 1) 100%);
+      background: linear-gradient(to right, rgba(59, 153, 156, 1) 0%, rgba(0, 177, 156, 1) 100%);
+      filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3b999c', endColorstr='#00b19c', GradientType=1);
 
-  }
-  .head{
-    width: 100%;
-    height: auto;
-    padding: 2em;
-  }
-  .conversation-list {
+    }
 
-    padding: 0px !important; }
+    .head {
+      width: 100%;
+      height: auto;
+      padding: 2em;
+    }
+
+    .conversation-list {
+
+      padding: 0px !important;
+    }
+
     .conversation-list .conversation-text {
       display: inline-block;
       float: left;
       font-size: 12px;
       margin-left: 5px;
-      width: 90%; }
-      .c-list li {
-        margin-bottom: 0px;
-        margin-top: 0px;
-       }
-    </style>
-  </head>
+      width: 90%;
+    }
 
-  <body>
+    .c-list li {
+      margin-bottom: 0px;
+      margin-top: 0px;
+    }
+  </style>
+</head>
 
-    <?php include '../lib/menu.php'; ?>
+<body>
+
+  <?php include '../lib/menu.php';?>
 
 
-    <div class="wrapper">
-      <div class="head">
-        <div class="row">
-          <div class="col-md-12">
-            <h2 class="text-light"><b>Inicio</b> <small><small>> Perfil Alumnos</small></small> </h2>
-          </div>
-          <div class="col-md-12">
-            <p class="text-light">Accede a tus calificaciones, entrega tareas y revisa tus pagos desde donde quieras</p>
-          </div>
+  <div class="wrapper">
+    <div class="head">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-light">
+            <b>Inicio</b>
+            <small>
+              <small>> Perfil Alumnos</small>
+            </small>
+          </h2>
+        </div>
+        <div class="col-md-12">
+          <p class="text-light">Accede a tus calificaciones, entrega tareas y revisa tus pagos desde donde quieras</p>
         </div>
       </div>
-      <div class="alert alert-success alert-dismissable" style="display:none;">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        Se han añadido las siguientes <a href="#" class="alert-link">funciones</a>:
-        <ul>
-          <li style="display:none;">Registro de inicio de sesión</li>
-          <li >Seguimiento sobre registro de notas</li>
-          <li style="display:none;">Ver ingreso de notas de asesor</li>
-        </ul>
-      </div>
-      <div class="container-fluid">
-        <br>
-        <!-- Page-Title -->
+    </div>
+    <div class="alert alert-success alert-dismissable" style="display:none;">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+      Se han añadido las siguientes
+      <a href="#" class="alert-link">funciones</a>:
+      <ul>
+        <li style="display:none;">Registro de inicio de sesión</li>
+        <li>Seguimiento sobre registro de notas</li>
+        <li style="display:none;">Ver ingreso de notas de asesor</li>
+      </ul>
+    </div>
+    <div class="container-fluid">
+      <br>
+      <!-- Page-Title -->
 
-        <!-- end page title end breadcrumb -->
-        <div class="row" >
-          <!-- CHAT -->
-          <div class="col-lg-12">
-            <div class="card-box">
-              <h4 class="m-t-0 m-b-20 header-title"><b>Tus Notas</b></h4>
-              <div class="col-md-12">
-                <div class="row">
-                  <div class="col-12 col-md-4 col-lg-4 ">
-                    <select id="stBloques" class=" form-control select2" name="">
-                      <?php
-                        for ($i=1; $i <=$bloqueencurso ; $i++) {
-                          $s="";
-                          if ($i==$bloqueencurso) {
-                            $s="selected";
-                          }else {
-                            $s="";
-                          }
-                          echo '<option '.$s.' value="'.$i.'">Bloque '.$i.'</option>';
-                        }
+      <!-- end page title end breadcrumb -->
+      <div class="row">
+        <!-- CHAT -->
+        <div class="col-lg-12">
+          <div class="card-box">
+            <h4 class="m-t-0 m-b-20 header-title">
+              <b>Tus Notas</b>
+            </h4>
+            <div class="col-md-12">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 ">
+                  <select id="stBloques" class=" form-control select2" name="">
+                    <?php
+for ($i = 1; $i <= $bloqueencurso; $i++) {
+    $s = "";
+    if ($i == $bloqueencurso) {
+        $s = "selected";
+    } else {
+        $s = "";
+    }
+    echo '<option ' . $s . ' value="' . $i . '">Bloque ' . $i . '</option>';
+}
 
-                       ?>
-                    </select>
-                  </div>
+?>
+                  </select>
                 </div>
+              </div>
 
-                <div class="row">
-                  <div id="main" class="col-md-12">
-                      
-                        
-                  </div>
+              <div class="row">
+                <div id="main" class="col-md-12">
+
+
                 </div>
               </div>
             </div>
-
-          </div> <!-- end col-->
-
-
+          </div>
 
         </div>
-      </div> <!-- end container -->
+        <!-- end col-->
+
+
+
+      </div>
     </div>
-    <!-- end wrapper -->
+    <!-- end container -->
+  </div>
+  <!-- end wrapper -->
 
 
-    <!-- Footer -->
-    <?php
-    include '../lib/foo.php';
-    require '../lib/scripts.php';
-    ?>
+  <!-- Footer -->
+  <?php
+include '../lib/foo.php';
+require '../lib/scripts.php';
+?>
 
     <!-- End Footer -->
     <!-- jQuery  -->
     <script type="text/javascript">
-    function bloques(){
-      var bloque=$('#stBloques').val();
-      var parametros = {
-        "b" : bloque,
-      };
-      $.ajax({
-        data:  parametros,
-        url:   'notasbloque.php',
-        type:  'GET',
-        beforeSend: function () {
-          var load='<div class="">'+
-            '<div class="text-center">'+
-              '<br>'+
-              '<img src="../../assets/images/loadcustom.gif" width="80" height="auto" alt=""><br>'+
-              '<h3 class="text-muted">Cargando Alumnos</h3>'+
-              '<div id="icon">'+
-                '<h5 class="text-muted">Esto puede tardar unos minutos</h5>'+
-              '</div>'+
-            '</div>'+
-          '</div>';
-          $('#main').html(load);
-        },
-        success: function (response) {
-          $("#main").html(response);
-        }
+      function bloques() {
+        var bloque = $('#stBloques').val();
+        var parametros = {
+          "b": bloque,
+        };
+        $.ajax({
+          data: parametros,
+          url: 'notasbloque.php',
+          type: 'GET',
+          beforeSend: function () {
+            var load = '<div class="">' +
+              '<div class="text-center">' +
+              '<br>' +
+              '<img src="../../assets/images/loadcustom.gif" width="80" height="auto" alt=""><br>' +
+              '<h3 class="text-muted">Cargando Alumnos</h3>' +
+              '<div id="icon">' +
+              '<h5 class="text-muted">Esto puede tardar unos minutos</h5>' +
+              '</div>' +
+              '</div>' +
+              '</div>';
+            $('#main').html(load);
+          },
+          success: function (response) {
+            $("#main").html(response);
+          }
+        });
+      }
+      $('#stBloques').change(function () {
+        bloques();
       });
-    }
-    $('#stBloques').change(function(){
       bloques();
-    });
-    bloques();
-  </script>
+    </script>
 
 </body>
+
 </html>
--- alumno/main/notasbloque.php
+++ alumno/main/notasbloque.php
@@ -3,11 +3,10 @@ require '../lib/sesion.php';
 require '../../conexion/conexion.php';
 require '../../assets/glib/isset.php';
 
-
-$bloque=d("b");
-$idgrado=get_query('idgrado');
-$sec=get_query('seccion');
-$idalumno=get_query('idalumno');
+$bloque = d("b");
+$idgrado = get_query('idgrado');
+$sec = get_query('seccion');
+$idalumno = get_query('idalumno');
 get_autorization($bloque);
 echo '<table class="table table-responsive table-hover" >
   <thead>
@@ -19,47 +18,47 @@ echo '<table class="table table-responsive table-hover" >
     </tr>
   </thead>
   <tbody>';
-$ssqqll="SELECT * FROM `materias` INNER JOIN `grados` ON materias.idgrado=grados.idgrado INNER JOIN `nombrematerias` ON materias.idnombremateria=nombrematerias.idnombremateria WHERE materias.idcole='$idcole' AND materias.idgrado='$idgrado' AND materias.seccion='$sec' ORDER BY num";
-$qquery=mysqli_query($conexion,$ssqqll);
-if ($qquery->num_rows==0) {
-  echo "Error no se encontraron materias";
-}else {
-  while ($e=mysqli_fetch_array($qquery)) {
-    $nclase=$e['nombreficha'];
-    $idnm=$e['idnombremateria'];
-    $ngrado=$e['boton'];
-    $num=$e['num'];
-    if ($nclase=="") {
-      $nclase=$e['nombre'];
-    }
-    $sqlnotas="SELECT *, SUM(obtenido) as tt FROM notas WHERE idcole='$idcole' AND idalumno='$idalumno' AND idmateria='$idnm' AND idbloque='$bloque' AND idgrado='$idgrado' GROUP BY idalumno";
-    $querynotas=mysqli_query($conexion,$sqlnotas);
-    if ($querynotas->num_rows==0) {
-      $total=0;
-    }
-    while ($f=mysqli_fetch_array($querynotas)) {
-      $total=$f["tt"];
-    }
-    $sqlnotas="SELECT *, SUM(obtenido) as tt FROM notasasesores WHERE idcole='$idcole' AND idalumno='$idalumno' AND idbloque='$bloque' AND idgrado='$idgrado' GROUP BY idalumno";
-    $querynotas=mysqli_query($conexion,$sqlnotas);
-    if ($querynotas->num_rows==0) {
-      $na=0;
-    }
-    while ($f=mysqli_fetch_array($querynotas)) {
-      $na=$f["tt"];
-    }
+$ssqqll = "SELECT * FROM `materias` INNER JOIN `grados` ON materias.idgrado=grados.idgrado INNER JOIN `nombrematerias` ON materias.idnombremateria=nombrematerias.idnombremateria WHERE materias.idcole='$idcole' AND materias.idgrado='$idgrado' AND materias.seccion='$sec' ORDER BY num";
+$qquery = mysqli_query($conexion, $ssqqll);
+if ($qquery->num_rows == 0) {
+    echo "Error no se encontraron materias";
+} else {
+    while ($e = mysqli_fetch_array($qquery)) {
+        $nclase = $e['nombreficha'];
+        $idnm = $e['idnombremateria'];
+        $ngrado = $e['boton'];
+        $num = $e['num'];
+        if ($nclase == "") {
+            $nclase = $e['nombre'];
+        }
+        $sqlnotas = "SELECT *, SUM(obtenido) as tt FROM notas WHERE idcole='$idcole' AND idalumno='$idalumno' AND idmateria='$idnm' AND idbloque='$bloque' AND idgrado='$idgrado' GROUP BY idalumno";
+        $querynotas = mysqli_query($conexion, $sqlnotas);
+        if ($querynotas->num_rows == 0) {
+            $total = 0;
+        }
+        while ($f = mysqli_fetch_array($querynotas)) {
+            $total = $f["tt"];
+        }
+        $sqlnotas = "SELECT *, SUM(obtenido) as tt FROM notasasesores WHERE idcole='$idcole' AND idalumno='$idalumno' AND idbloque='$bloque' AND idgrado='$idgrado' GROUP BY idalumno";
+        $querynotas = mysqli_query($conexion, $sqlnotas);
+        if ($querynotas->num_rows == 0) {
+            $na = 0;
+        }
+        while ($f = mysqli_fetch_array($querynotas)) {
+            $na = $f["tt"];
+        }
 
-    $total+=$na;
+        $total += $na;
 
-    if ($total<60) {
-      $c="danger";
-    }else {
-      $c="success";
-    }
-    echo "<tr>
-      <td>$num</td><td>$nclase </td>  <td><span class='badge badge-$c'>$total</span></td><td>".'<button  class="btn btn-secondary btn-sm"type="button" name="button">Ver Cuadro</button>'."</td>
+        if ($total < 60) {
+            $c = "danger";
+        } else {
+            $c = "success";
+        }
+        echo "<tr>
+      <td>$num</td><td>$nclase </td>  <td><span class='badge badge-$c'>$total</span></td><td>" . '<button  class="btn btn-secondary btn-sm"type="button" name="button">Ver Cuadro</button>' . "</td>
       <tr>";
-  }
+    }
 }
 echo "</tbody>
 </table>";
